0064cd17e946810dffa325e3cd1fcabdbeecda57
Sumit Kumar Jha <sumitjha5335@gmail.com>
2026-07-10 15:50:32 +0530

    Practicing Advance functions, setTimout, setInterval, forEach method

---
+++ scripts/12-todo-list-Using-forEach.js
@@ -0,0 +1,65 @@
+const todoList = [{
+        name: 'make dinner',
+        dueDate: '2026-07-06'
+    },
+    {
+        name: 'wash dishes',
+        dueDate: '2026-07-06'
+    }
+
+];
+
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHTML = '';
+
+    todoList.forEach(function(todoObject, index){
+        const {name, dueDate} = todoObject;
+        
+        const html = `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button class="delete-todo-button" onclick="
+                todoList.splice(${index}, 1);
+                renderTodoList();
+            ">Delete</button>
+        `;
+        todoListHTML += html;
+    });
+    // for(let i=0; i< todoList.length; i++){
+    //     const todoObject = todoList[i];
+    //     const {name, dueDate} = todoObject;
+        
+    //     const html = `
+    //         <div>${name}</div>
+    //         <div>${dueDate}</div>
+    //         <button class="delete-todo-button" onclick="
+    //             todoList.splice(${i}, 1);
+    //             renderTodoList();
+    //         ">Delete</button>
+    //     `;
+    //     todoListHTML += html;
+    // }
+    //console.log(todoListHTML);
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo(){
+    const inputElement = document.querySelector('.js-todo-input');
+    const name = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        name,
+        dueDate
+    });
+    //console.log(todoList);
+
+    inputElement.value = '';
+
+    renderTodoList();
+
+}
